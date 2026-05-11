9bb1c6a698f41d97935c52c570c386014d7ff0a1
Starkdel <107695274+Starkdel@users.noreply.github.com>
2026-05-11 15:58:23 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,11 @@ use App\Http\Controllers\AuthController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'Cloud Run updated successfully'
+    ]);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
