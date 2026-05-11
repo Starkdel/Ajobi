ead933181407020e974a62805c5eb49a4b67318e
Starkdel <107695274+Starkdel@users.noreply.github.com>
2026-05-11 21:52:50 +0100

    Update MailController.php

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+   public static function send_mail($user_d, $template){
+             Mail::send($template, $user_d, function($message)  use  ($user_d){
+                 $message->to($user_d['email'], $user_d['name'])->subject($user_d['title']);
+                 $message->from($user_d['sender_email'], $user_d['app_name']);
+             });
+
+    }
+    
+    
+  
+}
