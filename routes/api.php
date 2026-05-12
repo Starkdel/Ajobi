d73689db5ee4a91213aa944205ec9dab0565405e
Starkdel <107695274+Starkdel@users.noreply.github.com>
2026-05-12 12:35:51 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,12 @@ Route::post('/auth/login', [AuthController::class, 'login'] )->name('login');
 
 
 Route::post('/auth/register', [AuthController::class, 'register'] )->name('register');
+Route::post('/onboarding/step1', [AuthController::class, 'onboardingstep1'] )->name('onboardingstep1');
+Route::post('/onboarding/step2', [AuthController::class, 'onboardingstep2'] )->name('onboardingstep2');
+Route::post('/onboarding/step3', [AuthController::class, 'onboardingstep3'] )->name('onboardingstep3');
+Route::post('/onboarding/step4', [AuthController::class, 'onboardingstep4'] )->name('onboardingstep4');
+
+
 Route::get('/auth/google/redirect', [AuthController::class, 'redirect'])->name('auth.google.redirect');
 
 // Route to handle the callback from Google
