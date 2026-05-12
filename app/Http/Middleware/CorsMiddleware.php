d23eed4b5c96639c2e2b433d3f2f56876f0549d9
Starkdel <107695274+Starkdel@users.noreply.github.com>
2026-05-12 10:50:40 +0100

    Update CorsMiddleware.php

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,7 +15,7 @@ class CorsMiddleware
     {
         $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:5173');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://ajobi.onrender.com');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, x-xsrf-token');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
