29afb8840cd73b1da07849cb77d2570f96fb4e94
Starkdel <107695274+Starkdel@users.noreply.github.com>
2026-05-12 10:51:51 +0100

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['POST', 'GET'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' =>  [
+    'https://ajobi.onrender.com',
+    'https://www.ajobi.onrender.com',
+],
 
     'allowed_origins_patterns' => [],
 
