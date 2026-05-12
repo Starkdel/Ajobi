0108b15023b90fd7ec31e6cee78a8655f95b355d
Starkdel <107695274+Starkdel@users.noreply.github.com>
2026-05-12 19:45:11 +0100

    Update CorsMiddleware.php

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,18 +8,5 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CorsMiddleware
 {
-    /**
-     * Handle an incoming request.
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        $response = $next($request);
 
-        $response->headers->set('Access-Control-Allow-Origin', 'https://ajobi.onrender.com');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, x-xsrf-token');
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
-
-        return $response;
-    }
 }
