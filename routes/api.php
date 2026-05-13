a420a3c0b4527807820d5bab43c07eeb04d08788
Starkdel <107695274+Starkdel@users.noreply.github.com>
2026-05-13 13:58:09 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -39,7 +39,7 @@ Route::post('/onboarding/step2', [AuthController::class, 'onboardingstep2'] )->n
 Route::post('/onboarding/step3', [AuthController::class, 'onboardingstep3'] )->name('onboardingstep3');
 Route::post('/onboarding/step4', [AuthController::class, 'onboardingstep4'] )->name('onboardingstep4');
 Route::post('/onboarding/step5', [Ajoscorecontroller::class, 'calculateAjoScore'] )->name('calculateAjoScore');
-Route::get('/onboarding/progress', [AuthController::class, 'onboardingcheck'] )->name('onboardingcheck');
+Route::get('/onboarding/progress/{email}', [AuthController::class, 'onboardingcheck'] )->name('onboardingcheck');
 
 Route::get('/score/{userId}', [Ajoscorecontroller::class, 'getAjoreScore'] )->name('getAjoreScore');
 Route::get('/score/{userId}/events', [Ajoscorecontroller::class, 'ajoevents'] )->name('ajoevents');
