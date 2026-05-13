91a8dab6c55a4e3321198a4d679891e6a9290eae
Starkdel <107695274+Starkdel@users.noreply.github.com>
2026-05-13 19:53:43 +0100

    Update GroupController.php

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+use Twilio\Rest\Client;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\MailController;
+use Nette\Utils\Random;
+use Laravel\Socialite\Facades\Socialite;
+class AuthController extends Controller
+{
+
+public function creategroup(Request $request)
+{
+
+
+
+}
+
+
+}
