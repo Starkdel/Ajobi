cbd05f70e508d4cff5308bb94f79ef3e53f01364
Starkdel <107695274+Starkdel@users.noreply.github.com>
2026-05-15 15:03:13 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,7 @@ use Illuminate\Support\Facades\Validator;
 |
 */
 
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Route;
+
 
 Route::post('/virtual-account', function (Request $request) {
 
